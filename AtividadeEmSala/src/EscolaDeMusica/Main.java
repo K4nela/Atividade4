4c80268c4c711b9483e393ec42304efe486c292c
K4nela <mariaclara.clarinha1503@gmail.com>
2025-10-07 13:40:45 -0300

    update Main.java

--- AtividadeEmSala/src/EscolaDeMusica/Main.java
+++ AtividadeEmSala/src/EscolaDeMusica/Main.java
@@ -4,9 +4,17 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
-        InstrumentoMusical[] banda = new InstrumentoMusical[5];
+        InstrumentoMusical[] banda = new InstrumentoMusical[4];
 
-banda = new Violão ();
-
+        banda[0] = new Violao ("Violão Classico", "Madeira", 5);
+        banda[1] = new Saxofone ("Saxofone Alto", "Latão");
+        banda[2] = new Piano ("Piano de Cauda", "Madeira", 88);
+        banda[3] = new Bateria ("Bateria","Madeira",5);
+        
+        for(InstrumentoMusical ins: banda){
+            if(ins!=null){
+                ins.tocar();
+            }
+        }
     }
 }
